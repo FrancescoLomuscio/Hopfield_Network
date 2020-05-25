aa4f806e52fd26d16a2342b21244311784a04b63
unknown <francilomuscio@gmail.com>
2020-05-25 22:12:23 +0200

    Add fundamental memories

--- Hop_Net.py
+++ Hop_Net.py
@@ -7,30 +7,55 @@ Created on Mon May 18 19:20:49 2020
 
 import numpy as np
 from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+from PIL import Image
+
 
 def matrix2array(matrix):
     return matrix.flatten()
 
-def createWeightMatrix(imageAsArray):
-    n = np.shape(imageAsArray)[0]
+def createWeightMatrix(imagesAsArray):
+    n = np.shape(imagesAsArray[0])[0]
     weights = np.zeros((n,n))
-    for i in range(n):
-        for j in range(i,n):
-            if i != j:
-                weights[i,j] = imageAsArray[i] * imageAsArray[j]
-                weights[j,i] = weights[i,j]
-    return weights
+    for image in imagesAsArray:
+        image = matrix2array(image)
+        t = np.zeros((n,n))
+        for i in range(n):
+            for j in range(i,n):
+                if i != j:
+                    t[i,j] = image[i] * image[j]
+                    t[j,i] = t[i,j]
+        weights += t
+    return weights/len(imagesAsArray)
 
+def gray2binary(image):
+    gray = np.dot(image,[0.2989, 0.5870, 0.1140])
+    for i in range(gray.shape[0]):
+        for j in range(gray.shape[1]):
+            if gray[i,j] < 128:
+                gray[i,j] = -1
+            else:
+                gray[i,j] = 1
+
+    return gray
+
+def getFundamentalMemories():
+    digits = load_digits()
+    fm = [digits.images[0],digits.images[7]]
+    for image in fm:
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                if image[i,j] == 0:
+                    image[i,j] = -1
+                else:
+                    image[i,j] = 1
+    return fm
+        
 def main():
-    x = np.ones((2,2))
-    x = x + x
-    a = matrix2array(x)
-    y = np.ones((2,2))
-    a2 = matrix2array(y)
-    w = createWeightMatrix(a)
-    w += createWeightMatrix(a2)
-    w /= 2
+    memories = getFundamentalMemories()
+    w = createWeightMatrix(memories)
     print(w)
+    
 
 main()
     
